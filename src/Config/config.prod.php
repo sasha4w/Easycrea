21f4bf88c662fef5296feddcd4a43a7b5b443dbb
Sasha Rochedix <sasharochedix@gmail.com>
2024-10-18 11:30:57 +0200

    ajout config.php dans gitingore

--- src/Config/config.prod.php
+++ src/Config/config.prod.php
@@ -9,17 +9,17 @@ if (session_status() === PHP_SESSION_NONE) {
 /**
  * le DSN de la base
  */
-define('APP_DB_DSN', 'mysql:host=????.alwaysdata.net;dbname=?????;charset=UTF8');
+define('APP_DB_DSN', 'mysql:host=mysql-srochedix.alwaysdata.net;dbname=srochedix_reignswebapp;charset=UTF8');
 
 /**
  * le nom de l'utilisateur MYSQL
  */
-define('APP_DB_USER', '?????');
+define('APP_DB_USER', 'srochedix');
 
 /**
  * le mot de passe de l'utilisateur MYSQL
  */
-define('APP_DB_PASSWORD', '???????');
+define('APP_DB_PASSWORD', 'xQ2$gN3~lH3~qN2)');
 
 /**
  * le préfixe des tables dans la base (utile pour les bases partagées)
